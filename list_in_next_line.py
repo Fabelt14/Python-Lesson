244682aed6d8c50f7dc67e67cfabd863a6ab9398
Fabelt <78570215+Fabelt14@users.noreply.github.com>
2024-07-18 22:07:25 +0100

    listing integers on separate lines
    A program that creates a list with 
    the following integers: 3, 5, 9, 3, 2, 9, 10. 
    The program should iterate through the values 
    in the list and print out each of the integers 
    on separate lines.

---
+++ list_in_next_line.py
@@ -0,0 +1,10 @@
+'''Write a program that creates a list with 
+the following integers: 3, 5, 9, 3, 2, 9, 10. 
+The program should iterate through the values 
+in the list and print out each of the integers 
+on separate lines.'''
+
+list = [3, 5, 9, 3, 2, 9, 10]
+
+for num in list:
+    print(num)
