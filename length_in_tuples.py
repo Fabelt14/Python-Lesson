0bfa9567f855c05a9058856e723575deb26f1d2e
Fabelt <78570215+Fabelt14@users.noreply.github.com>
2024-07-18 22:09:34 +0100

    Printing lengths of strings in tuples
    A program that creates a tuple with the 
    following strings: "one", "two", "three", "four", 
    "five". The program should print out the length of 
    each string in the tuple on different lines

---
+++ length_in_tuples.py
@@ -0,0 +1,10 @@
+'''Write a program that creates a tuple with the 
+following strings: "one", "two", "three", "four", 
+"five". The program should print out the length of 
+each string in the tuple on different lines.
+'''
+
+nest = ("one", "two", "three", "four", "five")
+
+for strings in nest:
+    print(len(strings))
